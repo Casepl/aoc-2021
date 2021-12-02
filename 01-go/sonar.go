b4a349e873b340501b45128b5ec9ac46c16ef3e7
Sergey Scherbachenko <sshcherbachenko@avito.ru>
2021-12-02 21:43:27 +0300

    Update part 2

--- 01-go/sonar.go
+++ 01-go/sonar.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 )
 
-func main() {
+func part1() {
 	var max, prev int = 0, -1
 	file, err := os.Open("./input.txt")
 	if err != nil {
@@ -29,4 +29,54 @@ func main() {
 		prev = i
 	}
 	fmt.Println(max)
+}
+
+func part2() {
+	var answer int = 0
+
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	a, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	scanner.Scan()
+	b, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	scanner.Scan()
+	c, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	for scanner.Scan() {
+		d, err := strconv.Atoi(scanner.Text())
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		if b+c+d > a+b+c {
+			answer++
+		}
+		a = b
+		b = c
+		c = d
+	}
+	fmt.Println(answer)
+}
+
+func main() {
+	part1()
+	part2()
 }
